3b874d787393356f315fe09fc6fa1e2c3319ae44
Franky <franky.2021@scis.smu.edu.sg>
2023-06-15 17:23:16 +0800

    add primary key

--- models/mealplan.go
+++ models/mealplan.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 // MealPlan represents a meal plan structure
 type MealPlan struct {
 	gorm.Model
-	ID       int   `json:"id"`
+	ID       int   `json:"id" gorm:"primaryKey"`
 	Name     string   `json:"name"`
 	Description string `json:"description"`
 	Duration int      `json:"duration"`
