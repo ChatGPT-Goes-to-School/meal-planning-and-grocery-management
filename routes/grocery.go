ae14fc71a348e6826ef7e73ebf984a80576f5d24
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2023-06-20 13:03:06 +0800

    Update grocery.go

---
+++ routes/grocery.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/ChatGPT-Goes-to-School/meal-planning-and-grocery-management/handlers"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func SetupGroceryRoutes(router *gin.Engine, db *gorm.DB) {
+	groceryHandler := handlers.NewGroceryHandler(db)
+	// Initialize the handlers
+	router.GET("/groceries/:id", groceryHandler.GetGrocery)
+	router.GET("/groceries", groceryHandler.GetGroceriesByUsername)
+	router.POST("/groceries", groceryHandler.CreateGrocery)
+	router.PUT("/groceries/:id", groceryHandler.UpdateGrocery)
+	router.DELETE("/groceries/:id", groceryHandler.DeleteGrocery)
+	// Define more routes as needed
+}
