e8998c68cff2598b88ad56e1d39dbccbe4f83fc0
franky-lim24 <franky.2021@scis.smu.edu.sg>
2023-07-14 02:42:10 +0800

    Update mealplan.go

--- models/mealplan.go
+++ models/mealplan.go
@@ -1,20 +1,15 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 // MealPlan represents a meal plan structure
 type MealPlan struct {
 	gorm.Model
-	Name        string     `json:"name"`
-	Description string     `json:"description"`
-	Duration    int        `json:"duration"`
-	Meals       string     `json:"meals"`
-	Username    string     `json:"username"`
-	CreatedAt   *time.Time `gorm:"column:created_at;type:datetime"` // Specify the column type
-	UpdatedAt   *time.Time `gorm:"column:updated_at"`
-	DeletedAt   *time.Time `gorm:"column:deleted_at;index"` // Give a index name `deleted_at`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Duration    int    `json:"duration"`
+	Meals       string `json:"meals"`
+	Username    string `json:"username"`
 }
