04e516ab932b136ad433365ee71bcd8386bdf0bc
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2023-07-19 03:04:33 +0800

    add dateplan

--- models/mealplan.go
+++ models/mealplan.go
@@ -1,18 +1,21 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 // MealPlan represents a meal plan structure
 type MealPlan struct {
 	gorm.Model
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Duration    int    `json:"duration"`
-	Breakfast   string `json:"breakfast"`
-	Lunch       string `json:"lunch"`
-	Dinner      string `json:"dinner"`
-	Snack       string `json:"snack"`
-	Username    string `json:"username"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Duration    int       `json:"duration"`
+	Breakfast   string    `json:"breakfast"`
+	Lunch       string    `json:"lunch"`
+	Dinner      string    `json:"dinner"`
+	Snack       string    `json:"snack"`
+	Username    string    `json:"username"`
+	DatePlan    time.Time `json:"date"`
 }
